c9aa78c1895e4f7388809fd1ab918a2a5d6e88cb
Pandu Rani <ranipandu340@gmail.com>
2026-08-13 18:20:13 +0530

    jenkins

---
+++ 21-cicd-tools/variables.tf
@@ -0,0 +1,23 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z03884121IOCMF4WK7QMW"
+}
+
+variable "domain_name" {
+    default = "devopswithrani.online"
+}
+
+variable "sonar" {
+    default = true
+}
+
+variable "jenkins" {
+    default = true
+}
